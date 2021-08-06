cd93e7de9b246e6e0a76a86d365140466eca4968
Boyetuhin <tuhinroy881@gmail.comgit config --global user.name>
2021-08-07 00:19:45 +0530

    clean data fetch

--- lib/data.dart
+++ lib/data.dart
@@ -1,15 +1,15 @@
 class Data {
-  final int userId;
-  final int id;
+  final int name;
+  final int email;
   final String title;
   final String body;
 
-  Data({this.userId, this.id, this.title, this.body});
+  Data({this.name, this.email, this.title, this.body});
 
   factory Data.fromJson(Map<String, dynamic> json) {
     return Data(
-        userId: json['userId'],
-        id: json['id'],
+        name: json['name'],
+        email: json['email'],
         title: json['title'],
         body: json['body']);
   }
